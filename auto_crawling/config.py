57b63971abf33eb85ece36678afca8f173414e0f
joetae <138232620+joetae@users.noreply.github.com>
2025-02-08 03:44:02 +0900

    Delete auto_crawling directory

--- auto_crawling/config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-# 크롤링 설정
-review_count = 5  # 유사 리뷰 개수
-convert_count = 3  # 각 리뷰를 몇 번 변환할지
-
-# Chrome WebDriver 경로
-CHROME_DRIVER_PATH = "chromedriver"
--- auto_crawling/run.py
+++
@@ -1,5 +0,0 @@
-from crawler import AirbnbfyCrawler
-
-if __name__ == "__main__":
-    crawler = AirbnbfyCrawler()
-    crawler.run()
